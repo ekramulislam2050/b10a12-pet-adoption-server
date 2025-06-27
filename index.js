7a58a1a2f6db5c1ecfcb48cab53a53449a296adb
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-06-27 08:32:41 +0600

    geted allData

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
-const app=express()
-const cors =require('cors')
+const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require("jsonwebtoken")
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhpkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhpkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,6 +22,7 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -29,14 +30,45 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-     
+
+    // collections-----------------
+    const db = client.db("pet_adopt_nest")
+    const collectionsOfPets = db.collection("pets")
+
+    // verifyToken--------------------
+
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send("unAuthorization")
+      }
+      const token = req.headers.authorization.split(" ")[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
+        if (err) {
+          return res.status(403).send("forbidden access")
+        }
+        req.email = decoded;
+        next()
+      })
+    }
+
     // jwt---------------
-     app.post("/jwt",async(req,res)=>{
-          const userEmail = req.body;
-          console.log(userEmail)
-          const token=jwt.sign(userEmail,process.env.ACCESS_TOKEN_KEY,{expiresIn:'1h'})
-          res.send({token})
-     })
+    app.post("/jwt", async (req, res) => {
+      const userEmail = req.body;
+      console.log(userEmail)
+      const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_KEY, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
+    // allData -----------------
+    app.get("/allPet", async (req, res) => {
+      try {
+        const pets = await collectionsOfPets.find({}).toArray()
+        res.send(pets)
+      } catch (error) {
+        res.status(500).send({ error: error.message })
+      }
+    })
+   
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -45,9 +77,9 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get("/",async (req,res)=>{
-    res.send("welcome pet word")
+app.get("/", async (req, res) => {
+  res.send("welcome pet word")
 })
-app.listen(port,()=>{
-    console.log(`pet world is running : ${port}`)
+app.listen(port, () => {
+  console.log(`pet world is running : ${port}`)
 })
